a4ca51ebe7e88706849ce7ec3e40f4005e037504
Emirates <96430610+supertjq@users.noreply.github.com>
2023-09-19 12:02:34 +0800

    update

---
+++ kvraft/kvstatemachine_test.go
@@ -0,0 +1,19 @@
+package kvraft
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func TestBasic(t *testing.T) {
+	var kvstate KVStateMachine
+	kvstate = NewMKV()
+	for i:=0;i<10;i++{
+		kvstate.Put(strconv.Itoa(i),strconv.Itoa(i))
+	}
+	kvstate.Append(strconv.Itoa(6),"this is 6")
+	for i:=0;i<10;i++{
+		fmt.Println(kvstate.Get(strconv.Itoa(i)))
+	}
+}
